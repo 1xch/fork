385ea0da28c5b4f762489ce98034354a6c378fe5
blueblank <blueblank@gmail.com>
2015-02-17 11:42:27 -0500

    initial commit

---
+++ processors.go
@@ -0,0 +1,89 @@
+package fork
+
+import (
+	"fmt"
+	"reflect"
+)
+
+var errorType = reflect.TypeOf((*error)(nil)).Elem()
+
+func isValidater(typ reflect.Type) bool {
+	switch {
+	case typ.NumOut() == 1 && typ.Out(0) == errorType:
+		return true
+	}
+	return false
+}
+
+func isFilter(typ reflect.Type) bool {
+	switch {
+	case typ.NumOut() == 1:
+		return true
+	case typ.NumOut() == 2 && typ.Out(1) == errorType:
+		return true
+	}
+	return false
+}
+
+func canBeNil(typ reflect.Type) bool {
+	switch typ.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
+		return true
+	}
+	return false
+}
+
+func validate(fn reflect.Value, args ...interface{}) error {
+	validated, err := process(fn, args...)
+	if err != nil {
+		return err
+	}
+	return validated.(error)
+}
+
+func filter(fn reflect.Value, args ...interface{}) interface{} {
+	filtered, err := process(fn, args...)
+	if err != nil {
+		return err
+	}
+	return filtered
+}
+
+func process(fn reflect.Value, args ...interface{}) (interface{}, error) {
+	typ := fn.Type()
+	numIn := typ.NumIn()
+	var dddType reflect.Type
+	if typ.IsVariadic() {
+		if len(args) < numIn-1 {
+			return nil, fmt.Errorf("wrong number of args: got %d want at least %d", len(args), numIn-1)
+		}
+		dddType = typ.In(numIn - 1).Elem()
+	} else {
+		if len(args) != numIn {
+			return nil, fmt.Errorf("wrong number of args: got %d want %d", len(args), numIn)
+		}
+	}
+	argv := make([]reflect.Value, len(args))
+	for i, arg := range args {
+		value := reflect.ValueOf(arg)
+		// Compute the expected type. Clumsy because of variadics.
+		var argType reflect.Type
+		if !typ.IsVariadic() || i < numIn-1 {
+			argType = typ.In(i)
+		} else {
+			argType = dddType
+		}
+		if !value.IsValid() && canBeNil(argType) {
+			value = reflect.Zero(argType)
+		}
+		if !value.Type().AssignableTo(argType) {
+			return nil, fmt.Errorf("arg %d has type %s; should be %s", i, value.Type(), argType)
+		}
+		argv[i] = value
+	}
+	result := fn.Call(argv)
+	if len(result) == 2 && !result[1].IsNil() {
+		return result[0].Interface(), result[1].Interface().(error)
+	}
+	return result[0].Interface(), nil
+}
