a21c764b21093a050522f400e6766460970f9085
blueblank <blueblank@gmail.com>
2015-02-19 18:44:21 -0500

    eyeballing testing

--- form.go
+++ form.go
@@ -1,6 +1,9 @@
 package fork
 
-import "net/http"
+import (
+	"bytes"
+	"net/http"
+)
 
 type Form interface {
 	New() Form
@@ -12,4 +15,51 @@ type Former interface {
 	Process(*http.Request)
 	Render() string
 	Valid() bool
+}
+
+func NewForm(fs ...Field) *defaultform {
+	return &defaultform{fields: fs}
+}
+
+type defaultform struct {
+	//Validater
+	fields []Field
+}
+
+func (df *defaultform) New() Form {
+	var newform defaultform = *df
+	fs := df.fields
+	newform.fields = nil
+	for _, field := range fs {
+		newform.fields = append(newform.fields, field.New())
+	}
+	return &newform
+}
+
+func (df *defaultform) Fields(fs ...Field) []Field {
+	df.fields = append(df.fields, fs...)
+	return df.fields
+}
+
+func (df *defaultform) Process(r *http.Request) {
+	for _, fd := range df.Fields() {
+		fd.Set(r)
+	}
+}
+
+func (df *defaultform) Valid() bool {
+	for _, fd := range df.Fields() {
+		if !fd.Valid() {
+			return false
+		}
+	}
+	return true
+}
+
+func (df *defaultform) Render() string {
+	b := new(bytes.Buffer)
+	for _, fd := range df.Fields() {
+		_, _ = b.WriteString(fd.Render(fd))
+	}
+	return b.String()
 }
--- form_test.go
+++ form_test.go
@@ -6,16 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-
 	"testing"
-)
-
-var (
-	TestForm       Form  = NewTestForm()
-	TestListField  Field = ListField("listfield", makenewtextfield, TextField("zero"))
-	TestFormsField Field = FormsField("formfield", 2, SimpleForm())
-	//TestMulti2 []Field = []Field{TextField("four"), TextField("five"), TextField("six")}
-	//TestMulti3 []Field = []Field{TextField("seven"), TextField("eight"), TextField("nine")}
 )
 
 func makenewtextfield(name string, req *http.Request) Field {
@@ -25,70 +16,28 @@ func makenewtextfield(name string, req *http.Request) Field {
 }
 
 func SimpleForm() Form {
-	return &testform{
-		fields: []Field{
-			TextField("formfieldtext"),
-			BooleanField("formfieldbool", "FFbool", false),
-		},
-	}
+	return NewForm(TextField("formfieldtext"), BooleanField("formfieldbool", "FFbool", false))
 }
 
-func NewTestForm() Form {
-	return &testform{
-		fields: []Field{
-			//TextField("text"),
-			//TextField("email"),
-			//BooleanField("boolyes", "Yes", true),
-			//BooleanField("boolno", "No", false),
-			//HiddenField("invisible"),
-			//TextAreaField("textarea", 10, 10),
-			//TestListField,
-			TestFormsField,
-		},
-	}
+func NewTestForm(fds ...Field) Form {
+	return NewForm(fds...)
 }
 
-type testform struct {
-	//Validater
-	fields []Field
+func PerformGet(th *TestHandler) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	th.ServeHTTP(w, req)
+	return w
 }
 
-func (f *testform) New() Form {
-	var newform testform = *f
-	fs := f.fields
-	newform.fields = nil
-	for _, field := range fs {
-		newform.fields = append(newform.fields, field.New())
-	}
-	return &newform
-}
-
-func (f *testform) Fields(fs ...Field) []Field {
-	f.fields = append(f.fields, fs...)
-	return f.fields
-}
-
-func (f *testform) Process(r *http.Request) {
-	for _, fd := range f.Fields() {
-		fd.Set(r)
-	}
-}
-
-func (f *testform) Valid() bool {
-	for _, fd := range f.Fields() {
-		if !fd.Valid() {
-			return false
-		}
-	}
-	return true
-}
-
-func (f *testform) Render() string {
-	b := new(bytes.Buffer)
-	for _, fd := range f.Fields() {
-		_, _ = b.WriteString(fd.Render(fd))
-	}
-	return b.String()
+func PerformPost(th *TestHandler, values string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest("POST", "/", nil)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	v, _ := url.ParseQuery(values)
+	req.PostForm = v
+	w := httptest.NewRecorder()
+	th.ServeHTTP(w, req)
+	return w
 }
 
 type TestHandler struct {
@@ -106,82 +55,92 @@ func testserve() *TestHandler {
 	return &TestHandler{handlers: make(map[string]http.HandlerFunc)}
 }
 
-func testformget(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	t := TestForm.New()
-	out := bytes.NewBuffer([]byte(`<form action="/" method="POST">`))
-	out.WriteString(t.Render())
-	out.WriteString("</form>")
-	w.Write(out.Bytes())
+func getformhandlerfor(f Form) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		t := f.New()
+		out := bytes.NewBuffer([]byte(`test page delivered by GET`))
+		out.WriteString(`<form action="/" method="POST">`)
+		out.WriteString(t.Render())
+		out.WriteString("</form>")
+		out.WriteString("end test page")
+		w.Write(out.Bytes())
+	}
 }
 
-func testfieldrender(w http.ResponseWriter, r *http.Request) {
-	//w.WriteHeader(200)
-	//t := TestForm.New()
-	//out := bytes.NewBuffer([]byte(`<form action="/" method="POST">`))
-	//out.WriteString(t.Render())
-	//out.WriteString("</form>")
-	//w.Write(out.Bytes())
+func postformhandlerfor(f Form) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		t := f.New()
+		t.Process(r)
+		out := bytes.NewBuffer([]byte(`<form action="/" method="POST">`))
+		out.WriteString(t.Render())
+		out.WriteString("</form>")
+		w.Write(out.Bytes())
+	}
 }
 
-func testformpost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	t := TestForm.New()
-	t.Process(r)
-	//for _, fd := range t.Fields() {
-	//	fmt.Printf("%#v\n", fd.Get())
-	//}
-	out := bytes.NewBuffer([]byte(`<form action="/" method="POST">`))
-	out.WriteString(t.Render())
-	out.WriteString("</form>")
-	w.Write(out.Bytes())
-}
-
-func TestGetForm(t *testing.T) {
+func TestTextField(t *testing.T) {
 	ts := testserve()
-	ts.handlers["GET"] = testformget
-	req, _ := http.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	ts.ServeHTTP(w, req)
+
+	f := NewTestForm(TextField("text"))
+
+	ts.handlers["GET"] = getformhandlerfor(f)
+	ts.handlers["POST"] = postformhandlerfor(f)
+
+	w := PerformGet(ts)
+	fmt.Printf("%+v\n\n\n", w.Body)
+
+	w = PerformPost(ts, `text=TEXT`)
 	fmt.Printf("%+v\n\n\n", w.Body)
 }
 
-func TestPostForm(t *testing.T) {
+func TestBoolField(t *testing.T) {
 	ts := testserve()
-	ts.handlers["POST"] = testformpost
-	req, _ := http.NewRequest("POST", "/", nil)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	v, _ := url.ParseQuery(`testtext=TESTTEXT1&email=test@test.com&testboolyes=false&testboolno=true&invisible=unseen&textareatest='4scoreN7yearsago'&listfield-0-zero='Iam0'&listfield-1-one='Iam1'&formfield-0-formfieldtext-0=textinaformfieldtextfield&formfield-0-formfieldbool-1=false`)
-	//fmt.Printf("%+v\n", v)
-	req.PostForm = v
-	w := httptest.NewRecorder()
-	ts.ServeHTTP(w, req)
-	fmt.Printf("%+v\n\n\n", w.Body)
-}
 
-func TestPostListField(t *testing.T) {
-	ts := testserve()
-	ts.handlers["POST"] = testformpost
-	req, _ := http.NewRequest("POST", "/", nil)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	v, _ := url.ParseQuery(`listfield-0-zero='Iam0'&listfield-1-one='Iam1'`)
-	//fmt.Printf("%+v\n", v)
-	req.PostForm = v
-	w := httptest.NewRecorder()
-	ts.ServeHTTP(w, req)
+	f := NewTestForm(BooleanField("yes", "YES", true), BooleanField("no", "NO", false))
+
+	ts.handlers["GET"] = getformhandlerfor(f)
+	ts.handlers["POST"] = postformhandlerfor(f)
+
+	w := PerformGet(ts)
+	fmt.Printf("%+v\n\n\n", w.Body)
+
+	w = PerformPost(ts, `yes=false&no=true`)
 	fmt.Printf("%+v\n\n\n", w.Body)
 
 }
 
-func TestPostFormFields(t *testing.T) {
+var ListField1 Field = ListField("listfield", makenewtextfield, TextField("zero"))
+
+func TestListField(t *testing.T) {
 	ts := testserve()
-	ts.handlers["POST"] = testformpost
-	req, _ := http.NewRequest("POST", "/", nil)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	v, _ := url.ParseQuery(`formfield-0-formfieldtext-0=FORMFIELD0textfield&formfield-0-formfieldbool-1=true&formfield-1-formfieldtext-0=formfield1text&formfield-1-formfieldbool-1=false`)
-	//fmt.Printf("%+v\n", v)
-	req.PostForm = v
-	w := httptest.NewRecorder()
-	ts.ServeHTTP(w, req)
+
+	f := NewTestForm(ListField1)
+
+	ts.handlers["GET"] = getformhandlerfor(f)
+	ts.handlers["POST"] = postformhandlerfor(f)
+
+	w := PerformGet(ts)
+	fmt.Printf("%+v\n\n\n", w.Body)
+
+	w = PerformPost(ts, `listfield-0-zero=IamZERO&listfield-1-one=IamONE&listfield7-seven=IshouldnotbeSEVEN`)
+	fmt.Printf("%+v\n\n\n", w.Body)
+}
+
+var FormsField1 Field = FormsField("formfield", 2, SimpleForm())
+
+func TestFormsField(t *testing.T) {
+	ts := testserve()
+
+	f := NewTestForm(FormsField1)
+
+	ts.handlers["GET"] = getformhandlerfor(f)
+	ts.handlers["POST"] = postformhandlerfor(f)
+
+	w := PerformGet(ts)
+	fmt.Printf("%+v\n\n\n", w.Body)
+
+	w = PerformPost(ts, `formfield-0-formfieldtext-0=FORMFIELD0&formfield-0-formfieldbool-1=true&formfield-1-formfieldtext-0=FORMFIELD1&formfield-1-formfieldbool-1=false`)
 	fmt.Printf("%+v\n\n\n", w.Body)
 }
